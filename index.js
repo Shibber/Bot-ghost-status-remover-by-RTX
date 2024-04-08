2f0f1681e9bda4a1a833d14bb37f252b99b79c99
Shibber <166437296+Shibber@users.noreply.github.com>
2024-04-08 21:43:42 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["hugs for you","by exodus"];
 
 
 let currentIndex = 0;
